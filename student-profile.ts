f9b49ef2b93fbb43d75c1f36d704c1226cbf1130
Elin Hedin-Larsson <elinhedinlarsson3@gmail.com>
2026-08-26 13:24:03 +0200

    complete second assignment

--- student-profile.ts
+++ student-profile.ts
@@ -1,17 +1,25 @@
-const firstName: string = "Kilian";
-const age: number = 17;
-const isEnrolled = true;
+// const firstName: string = "Kilian";
+// const age: number = 17;
+// const isEnrolled = true;
 
-const describeStudent = (firstName: string, age: number) => {
-    return `${firstName} is ${age} years old. `;
-}
-console.log(describeStudent(firstName, age));
+// const describeStudent = (firstName: string, age: number) => {
+//     return `${firstName} is ${age} years old. `;
+// }
+// console.log(describeStudent(firstName, age));
 
 
-const formatGreeting = (name: string, formal?: boolean) => {
-    if (formal) {
-        return `Good day, ${name}`;
-    }
-    return `Hi, ${name}`;
-}
-console.log(formatGreeting("Kilian", true));
+// const formatGreeting = (name: string, formal?: boolean) => {
+//     if (formal) {
+//         return `Good day, ${name}`;
+//     } 
+//     return `Hi, ${name}`;
+// }
+// console.log(formatGreeting("Kilian"));
+
+const ages :number []= [2,20,5,40,100]
+const agesInFiveYears=ages.map(age=> age+10);
+console.log(agesInFiveYears);
+
+const names:string [] = ["Kilian", "Leana", "Cleo" ,"Hero", "Wio"];
+const shortNames = names.filter((names) =>names.length<=4);
+console.log(shortNames);
