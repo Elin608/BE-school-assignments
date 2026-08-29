9c6d0144bccd84c868ff88efd36c67d271d392bb
Elin Hedin-Larsson <elinhedinlarsson3@gmail.com>
2026-08-29 10:27:11 +0200

    Add interfaces and person objects

--- student-profile.ts
+++ student-profile.ts
@@ -23,18 +23,59 @@
 // const names:string [] = ["Kilian", "Leana", "Cleo" ,"Hero", "Wio"];
 // const shortNames = names.filter((names) =>names.length<=4);
 // console.log(shortNames);
-const scores:number[]=[60,82,20,14,12,4,66,23]
+// const scores:number[]=[60,82,20,14,12,4,66,23]
 
-const highScore=scores.filter((scores)=> scores >=50);
-console.log(highScore);
+// const highScore=scores.filter((scores)=> scores >=50);
+// console.log(highScore);
 
-const grades = highScore.map((score) =>  
-score >= 80? "A":
-score>= 70? "B":
-score>= 60? "C": "F"
+// const grades = highScore.map((score) =>  
+// score >= 80? "A":
+// score>= 70? "B":
+// score>= 60? "C": "F"
 
-);
-console.log(grades); 
+// );
+// console.log(grades); 
 
-const failed = scores.filter ((score) =>score<50);
-console.log(failed.length);
+// const failed = scores.filter ((score) =>score<50);
+// console.log(failed.length);
+
+// interface Book {
+//     title: string,
+//     author: string,
+//     pages: number,
+// }
+// const myBook:Book= {
+//     title: "The Housemaid",
+//     author: "Freida McFadden",
+//     pages: 328,
+
+// }
+// console.log(myBook.title);
+interface Adress{
+    city:string;
+    postalCode?:string;
+}
+interface Person{
+    name:string;
+    age:number;
+    adress:Adress;
+}
+const person1:Person = {
+    name: "Johanna",
+    age:10,
+    adress:{
+        city: "Madrid",
+        postalCode: "03189"
+    }
+}
+const person2:Person ={
+    name:"Joakim",
+    age:12,
+    adress:{
+        city: "Alicante",
+        postalCode: "031222",
+    }
+}
+console.log(person1.adress.city);
+console.log(person2.adress.city);
+
